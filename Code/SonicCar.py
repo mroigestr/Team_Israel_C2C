b8c609f42c2e4d1f3c6e0554dac0f9ac577ce6c2
imm-81 <imm-81@gmx.de>
2022-03-24 15:53:57 +0100

    Aufgabe 4

--- Code/SonicCar.py
+++ Code/SonicCar.py
@@ -25,26 +25,16 @@ class SonicCar(bc.BaseCar):
         self.fw = bc.bk.Front_Wheels()
         self.bw = bc.bk.Back_Wheels()
         self.us = bc.bk.Ultrasonic()
-        self._too_close = 10 
+        self._too_close = 10
+        self.global_data =[]
 
+    # #@property
+    # def too_close(self):
+    #     return self._too_close
 
-    @property
-    def too_close(self):
-        return self._too_close
-
-    # @too_close.setter
-    # def too_close(self, too_close):
-    #     if too_close < 10:
-    #         print("Warnwert muss >= 10 sein und nicht {}!".format(too_close))
-    #         print("Warnwert wird auf 10 cm gesetzt!")
-    #         self._too_close = 10
-    #     else:
-    #         self._too_close = too_close
-
-
-    def too_close(self, speed):
-        self.too_close = speed/3
-
+    #@too_close.setter
+    def mt_too_close(self, speed, factor: int = 1):
+            self.too_close = int(factor*(speed/3))
 
     def driving_data(self,dist):
         t_now = datetime.utcnow().strftime('%H:%M:%S.%f')[:-3]
@@ -55,30 +45,13 @@ class SonicCar(bc.BaseCar):
         with open("Log-Datei.csv", mode ='w') as log_file:
             write = csv.writer(log_file)
             write.writerow(headerList)
-            write.writerows(self.global_data)
-
- 
-        # #import os
-        # headers = ['head1', 'head2']
-        # for row in interator:
-        #     with open('file.csv', 'a') as f:
-        #         file_is_empty = os.stat('file.csv').st_size == 0
-        #         writer = csv.writer(f, lineterminator='\n')
-        #         if file_is_empty:
-        #             writer.writerow(headers)
-        #         writer.writerow(row)
-
-
-        
-        # display csv file
-        
-    
+            write.writerows(self.global_data)    
     
     def obstacle(self, too_close):
         """
         Ermittelt Abstand und unterbricht bei Wert < "too_close"
         """
-        self.global_data =[] 
+        #self.global_data =[] 
         while True:
             bc.bk.time.sleep(0.1)
             dist = self.us.distance()
@@ -88,24 +61,51 @@ class SonicCar(bc.BaseCar):
                 print("Achtung!", dist, "cm")
                 self.stop()
                 break
-
-
-
     
-    def Fahrparcours_3(self) -> None:
+    def data_rec(self):
+       while True:
+            time.sleep(0.1)
+            dist = self.us.distance()
+            data = self.driving_data(dist)
+            self.global_data.append(data)
+            break
+   
 
+    def Fahrparcours_3(self) -> None:        
         self.drive(60, 1, 90)
         #self.too_close = -4
-        self.too_close(self.speed)
-        print(self.too_close)
+        print(self.speed)
+        print(type(self.speed))
+        self.mt_too_close(self.speed)
+        #too_close = self.speed/3
+        #print(self.too_close)
         self.obstacle(self.too_close)
 
+    def RV(self) -> None:        
+        self.drive(30, -1, 135)
+        for i in range(20):
+            time.sleep(0.1)
+
+            dist = self.us.distance()
+            data = self.driving_data(dist)
+            self.global_data.append(data)           
+        
+
+    def Fahrparcours_4(self) -> None:   
+        #self.data_rec()
+        self.Fahrparcours_3()
+        #self.drive(30, -1, 135)
+        self.RV()
+        #time.sleep(2)
+        #self.stop()        
+        self.Fahrparcours_3()
+        #self.drive(30, 1, 90)
         
 def main():
+    
     sc = SonicCar()
-    sc.Fahrparcours_3()
+    sc.Fahrparcours_4()
     sc.write_to_csv()
-
 
 if __name__ == '__main__':
     main()
