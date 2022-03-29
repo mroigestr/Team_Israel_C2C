4a4379742b3d607d1828de9ae9417617f02b3815
ModiNull <modrowmi@gmail.com>
2022-03-29 15:21:57 +0200

    Test gitignore

---
+++ Code/Null-32-bin.py
@@ -0,0 +1,10 @@
+from matplotlib.pyplot import text
+
+
+z_dez = 32
+for i in range(z_dez):
+    b_dez = ''.join(bin(i).split("0b")
+    
+    print(b_dez, type(b_dez))
+    print(b_dez.zfill(5))
+    # print(b_dez)
