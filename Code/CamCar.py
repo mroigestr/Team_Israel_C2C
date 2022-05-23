b004b14c0e23334adabc2fc6c85f8791b6be01d6
modiNull <modrowmi@gmail.com>
2022-05-23 13:09:52 +0200

    CamCar MM

--- Code/CamCar.py
+++ Code/CamCar.py
@@ -27,39 +27,46 @@ class CamCar(object):
             upper = np.array([120, 255, 255])
             mask = cv.inRange(img_hsv, lower, upper) # mask ist Numpy-Array
 
-            # image_hough = self.line_detection(mask)
+            # mask_cn = cv.Canny(img_hsv, 50, 200)
+            mask_cn = cv.Canny(mask, 50, 100)
+
+            image_hough = self.line_detection(mask)
+            # image_hough = self.line_detectP(mask)
 
             # Visualisierung des Bilds
-            cv.imshow("Display window (press q to quit)", mask)
-            #cv.imshow("Display window (press q to quit)", image_hough)
+            # cv.imshow("Display window (press q to quit)", mask)
+            cv.imshow("Display window (press q to quit)", image_hough)
             # Ende bei Drücken der Taste q
             if cv.waitKey(1) == ord('q'):
                 break
         # Kamera-Objekt muss "released" werden, um "später" ein neues Kamera-Objekt erstellen zu können!!!
-        self.release()
+        self.Cam.release()
     
-    def line_detection(self, image):
+    def line_detection(self, mask):
         # Klassische Hough-Transformation
         rho = 1  # distance precision in pixel, i.e. 1 pixel
-        angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
-        min_threshold = 100  # minimal of votes, Je geringer Min_threshold, dest mehr Geraden werden erkannt.
+        angle = np.pi / 180   # angular precision in radian, i.e. 1 degree
+        min_threshold = 80  # minimal of votes, Je geringer Min_threshold, dest mehr Geraden werden erkannt.
 
-        parameter_mask = cv.HoughLines(image, rho, angle, min_threshold)
-        print("Shape Parameter_mask:",parameter_mask.shape)
+        parameter_mask = cv.HoughLines(mask, rho, angle, min_threshold)
+        print("Shape Parameter_mask:", parameter_mask.shape)
         
-
-        img2 = image.copy()
+        img2 = mask.copy()
         img2 = cv.cvtColor(img2, cv.COLOR_GRAY2RGB)
         for line in parameter_mask:
             rho,theta = line[0]
-            a = -np.cos(theta)/np.sin(theta) # Anstieg der Gerade
-            b = rho/np.sin(theta)            # Absolutglied/Intercept/Schnittpunkt mit der y-Achse
+            if abs(np.sin(theta)) < 1e-6:
+                a = 1000
+                b = 1000
+            else:
+                a = -np.cos(theta)/np.sin(theta) # Anstieg der Gerade
+                b = rho/np.sin(theta)            # Absolutglied/Intercept/Schnittpunkt mit der y-Achse
             x1 = 0
             y1 = int(b)
             x2 = 1000
             y2 = int(a*1000+b)
             #print(x1,x2,y1,y2)
-            img2=cv.line(img2,(x1,y1),(x2,y2),(200,100,100),1) # adds a line to an image
+            img2 = cv.line(img2, (x1,y1), (x2,y2), (200,100,100), 1) # adds a line to an image
             cv.putText(img2, 
                 text = 'erkannte Geraden',
                 org=(10,190), # Position
@@ -70,6 +77,18 @@ class CamCar(object):
         return img2
         
 
+    def line_detectP(self, mask):
+        # Probalistische Hough-Transformation
+        rho = 1  # distance precision in pixel, i.e. 1 pixel
+        angle = np.pi / 180  # angular precision in radian, i.e. 1 degree
+        min_threshold = 10   # in etwa Anzahl der Punkte auf der Geraden. Je geringer Min_threshold, dest mehr Geraden werden erkannt.
+        minLineLength = 8    # Minimale Linienlänge
+        maxLineGap = 4       # Maximale Anzahl von Lücken in der Linie
+
+        line_segments = cv.HoughLinesP(mask, rho, angle, min_threshold, np.array([]), minLineLength=minLineLength, maxLineGap=maxLineGap)
+        print(line_segments.shape)
+        # Elemente stellen Punkte des Liniensegmentes dar (x1,y1,x2,y2)
+        line_segments[:2]
 
 
 def main():
